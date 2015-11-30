0a77b752a551950e05d51535fcc24cac73293e98
chenzhiwei <chenzhiwei@zhlink.com>
2015-11-30 20:33:08 +0800

    asdf

---
+++ src/main/java/com/twocater/diamond/ext/dayou/AccessServerHandlerFactory.java
@@ -0,0 +1,31 @@
+package com.twocater.diamond.ext.dayou;
+
+import com.twocater.diamond.core.netty.AbstractHandlerFactory;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+
+/**
+ * Created by chenzhiwei on 15-11-30.
+ */
+public class AccessServerHandlerFactory extends AbstractHandlerFactory {
+    @Override
+    public ChannelInboundHandlerAdapter createHandler() {
+        return null;
+    }
+
+    @Override
+    public ChannelInboundHandlerAdapter[] createDecoder() {
+        return new ChannelInboundHandlerAdapter[0];
+    }
+
+    @Override
+    public ChannelOutboundHandlerAdapter[] createEncoder() {
+        return new ChannelOutboundHandlerAdapter[0];
+    }
+
+    @Override
+    public ChannelHandlerAdapter createServerHandler() {
+        return null;
+    }
+}
