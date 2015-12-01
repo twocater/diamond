535e15e98c5a4454d14caabfb662cfca612ef6ad
chenzhiwei <chenzhiwei@zhlink.com>
2015-12-01 20:46:22 +0800

    asdf

--- src/main/java/com/twocater/diamond/ext/dayou/DayouDecoder.java
+++ src/main/java/com/twocater/diamond/ext/dayou/DayouDecoder.java
@@ -1,7 +1,25 @@
 package com.twocater.diamond.ext.dayou;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
 /**
  * Created by chenzhiwei on 15-11-30.
  */
-public class DayouDecoder {
+public class DayouDecoder extends ByteToMessageDecoder {
+
+    @Override
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+
+        if (byteBuf.readableBytes() <4) {
+            ///
+            return;
+        }
+        byte b1 = byteBuf.readByte();
+        byte b2 = byteBuf.readByte();
+        byte b3 = byteBuf.readBytes(2);
+    }
 }
